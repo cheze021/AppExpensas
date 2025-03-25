abe07f232b32d16cc1a3c55e027cc3cc4607b129
Ezequiel Introini <chezemanya@gmail.com>
2025-03-25 16:28:11 -0300

    Unit Test: - Dependencia para unit test - Creados test de ejemplo y funcionales para el Repo

---
+++ composeApp/src/commonTest/kotlin/ExampleTest.kt
@@ -0,0 +1,81 @@
+import model.Expense
+import model.ExpenseCategory
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class ExampleTest {
+
+    @Test
+    fun sum_must_succeed() {
+        // Given
+        val x = 5
+        val y = 10
+
+        // When
+        val result = x + y
+
+        // Then
+        assertEquals(15, result)
+    }
+
+    @Test
+    fun sum_must_fail() {
+        // Given
+        val x = 5
+        val y = 10
+
+        // When
+        val result = x + y
+
+        // Then
+        assertNotEquals(10, result)
+    }
+
+    @Test
+    fun expense_model_list(){
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.50, category = ExpenseCategory.CAR, description = "Combustible")
+
+        // When
+        expenseList.add(expense)
+
+        // Then
+        assertContains(expenseList, expense)
+    }
+
+    @Test
+    fun expense_model_param_success() {
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.50, category = ExpenseCategory.OTHER, description = "Combustible")
+        val expense2 = Expense(id = 2, amount = 8.340, category = ExpenseCategory.OTHER, description = "Limpieza")
+
+        // When
+        expenseList.add(expense)
+        expenseList.add(expense2)
+
+        // Then
+        assertEquals(expense.category, expense2.category)
+        assertEquals(expenseList[0].category, expenseList[1].category)
+    }
+
+    @Test
+    fun expense_model_param_fail() {
+        // Given
+        val expenseList = mutableListOf<Expense>()
+        val expense = Expense(id = 1, amount = 4.50, category = ExpenseCategory.CAR, description = "Combustible")
+        val expense2 = Expense(id = 2, amount = 8.340, category = ExpenseCategory.OTHER, description = "Limpieza")
+
+        // When
+        expenseList.add(expense)
+        expenseList.add(expense2)
+
+        // Then
+        assertNotEquals(expense.category, expense2.category)
+        assertNotEquals(expenseList[0].category, expenseList[1].category)
+    }
+
+}
