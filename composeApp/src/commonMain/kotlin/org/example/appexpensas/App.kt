95c98fa6ad0580581672e37fe308d560c0fcb8ec
Ezequiel Introini <chezemanya@gmail.com>
2025-04-17 18:31:00 -0300

    Feature Menu Lateral: - Icono para cerrar menu lateral

--- composeApp/src/commonMain/kotlin/org/example/appexpensas/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/appexpensas/App.kt
@@ -1,7 +1,10 @@
 package org.example.appexpensas
 
 import AppTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.FloatingActionButton
@@ -16,6 +19,7 @@ import androidx.compose.material.icons.filled.AddBox
 import androidx.compose.material.icons.filled.Apps
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.GraphicEq
 import androidx.compose.material.icons.filled.List
@@ -31,6 +35,7 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -184,7 +189,29 @@ fun AppDrawer(
     val colors = getColorsTheme()
 
     ModalDrawerSheet {
-        Text("Menú", modifier = Modifier.padding(16.dp), fontSize = 20.sp)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Menú",
+                color = colors.textColor,
+                fontSize = 20.sp
+            )
+
+            IconButton(
+                onClick = { closeDrawer() }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Cerrar menú",
+                    tint = Color.Black
+                )
+            }
+        }
 
         HorizontalDivider(color = Color.LightGray, thickness = 1.dp)
 
